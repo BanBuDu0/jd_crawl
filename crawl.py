e1222be334df1bb789c16cd43d4aa8fd47f5e841
syj <1171871069@qq.com>
2019-04-28 14:49:53 +0800

    update
    update

--- crawl.py
+++ crawl.py
@@ -10,7 +10,8 @@ import time
 def getHTML(u, data=None):
     headers = {
         'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+        'Referer': 'https://item.jd.com'
     }
     r = requests.get(u, params=data, headers=headers)
     r.encoding = r.apparent_encoding
--- test1.py
+++ test1.py
@@ -10,10 +10,12 @@ import time
 def getHTML(u, data=None):
     headers = {
         'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+        'Referer': 'https://item.jd.com'
     }
     r = requests.get(u, params=data, headers=headers)
     r.encoding = r.apparent_encoding
     print(r.text)
 
-getHTML("https://search.jd.com/Search?keyword=gtx1050")
+getHTML("https://club.jd.com/comment/skuProductPageComments.action?callback=fetchJSON_comment98vv46561\
+    &productId=1686632&score=0&sortType=5&page=0&pageSize=10&isShadowSku=0&fold=1")
